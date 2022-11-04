5b078b3b084d5b0b1b40b75a330df9253940dc3c
Muelp014 <chayakornpluem4u@gmail.com>
2022-11-04 10:44:42 +0700

    first commit

---
+++ components/routes/index.js
@@ -0,0 +1,12 @@
+import Login from "../components/Login/index.js";
+
+
+const routes = [
+    {
+        path: '/Login',
+        component: Login,
+        exact: true
+    }
+]
+
+export default routes;
